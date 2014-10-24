64d79c534242307eee3679eb4a5e0b834510a347
unknown <vineet.kumar@KTCCOWHJ5FZQ1.kytc.ds.ky.gov>
2014-10-24 11:45:08 -0400

    Assignment2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,59 @@
-## Put comments here that give an overall description of what your
-## functions do
+## the first function( makeCacheMatrix) will retrun the list 
+## and store the matrix elements. 
+## Function get : To get Matrix elements 
+## Function Set : To get Matrix elments. This will be use to verify if elements are same
+##			before get the cached Inverse value.  
+## Function getinverse : To get the cached Matrix inverse
+## Function getinverse : To set the Matrix inverse in cache.
 
-## Write a short comment describing this function
+
+## CacheMatrix to create the matrix. 
 
 makeCacheMatrix <- function(x = matrix()) {
-
+	m<-NULL
+	set<-function(y){
+		x<<-y
+		m<<-NULL
+		}
+	get <- function() x
+	setinverse <-function(inverse) m<<- inverse
+	getinverse <-function() m
+	list(set = set, 
+		get =get, 
+	     	setinverse = setinverse, 
+		getinverse =getinverse)
 }
 
 
-## Write a short comment describing this function
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
+## Following function will retrun the cache inverse of matrix only if elments are same and
+## inverse is not null.otherwise it will calculate the inverse with Solve function 
+## and then set the new value in cache again.
+
+
+cacheSolve <- function(x, newMatrix=matrix(),...) {
+      ## Get the cached data to make sure it's same as passed matrix x
+	
+	cached_data<-x$get()
+	
+	## the following If statement will check if all elements are same 
+	if (all(cached_data==newMatrix)) 
+		{	
+			## All elements are same and now get the saved inverse.
+			m<-x$getinverse()
+			if(!is.null(m)){	
+				print("Getting Cache inverse of Matrix")
+				return(m)
+				}
+			m<- solve(cached_data)
+			x$setinverse(m)
+			m
+		}
+	else
+		{	## Elements are not same recalculate inverse 
+			## of the Matix elements and set the cache value
+			m<- solve(newMatrix, ...)
+			x$setinverse(m)
+			m
+		}
+	}
